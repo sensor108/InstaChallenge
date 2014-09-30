7aa1000533ce33f0d7645a3daaf1e3602e276b6a
sensor108 <sensor@sensors-mbp.fritz.box>
2014-09-30 12:38:06 +0200

    Initial Commit

---
+++ InstaChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaChallenge
+//
+//  Created by sensor108 on 30.09.14.
+//  Copyright (c) 2014 René Nicklas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaChallenge
+//
+//  Created by sensor108 on 30.09.14.
+//  Copyright (c) 2014 René Nicklas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
